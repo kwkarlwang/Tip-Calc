1550d85c50c7d106639cc97946f6677a9eb2e0df
Karl Wang <kwkarlwang@gmail.com>
2019-02-10 14:11:03 -0800

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Karl Wang on 2/10/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
